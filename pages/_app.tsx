78123a120fb9995cf3a6934e9b471d070165fd55
Ida Jimenez <ida@old.st>
2022-06-12 13:52:48 +0800

    sample commit with console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    console.log('ok');
+
     return <Component {...pageProps} />;
 }
 
